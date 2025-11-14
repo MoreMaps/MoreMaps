f7442d11636ad2404ad21372f3245a625ae7f534
al426211 <128591824+al426211@users.noreply.github.com>
2025-11-14 01:23:57 +0100

    test(it01): add and modify acceptance tests for testing users

--- src/app/acceptance/test-data.ts
+++ src/app/acceptance/test-data.ts
@@ -1,19 +1,17 @@
 // Revisar estos datos conforme avance el proyecto para que reflejen un objeto real
-import {gender, UserModel} from '../data/UserModel'
+import {UserModel} from '../data/UserModel'
 
-export const MOCK_USERS: UserModel[] = [
-    {email: "ramonejemplo@gmail.com", password: "Passw0rd!", nombre: "Ramón", apellidos: "García García", genero: gender.MALE},
-    {email: "mariaejemplo@gmail.com", password: "P4ssword!", nombre: "María", apellidos: "De Los Campos", genero: gender.FEMALE},
+export const USER_TEST_DATA = [
+    {email: "ramonejemplo@gmail.com", pwd: "Passw0rd!", nombre: "Ramón", apellidos: "García García"},
+    {email: "mariaejemplo@gmail.com", pwd: "P4ssword!", nombre: "María", apellidos: "De Los Campos"},
 ]
 
-// En historias posteriores habrá que poner el nombre de las interfaces que modelan estos requisitos de datos
-
-export const MOCK_POI = [
+export const POI_TEST_DATA = [
     {latitud: 38.345170, longitud: -0.481490, toponimo: "Alicante", alias: "", descripcion: "Ciudad que no frecuento"},
     {latitud: 39.473910, longitud: -0.376388, toponimo: "Valencia", alias: "València", descripcion: "Ciudad que frecuento"},
     {latitud: 39.985980, longitud: -0.037438, toponimo: "Castellón de la Plana", alias: "Castellón", descripcion: "Mi ciudad que no frecuento"},
 ]
 
-export const MOCK_VEHICLE = [
+export const VEHICLE_TEST_DATA = [
     {alias: "Ford Fiesta", matricula: "1234XYZ", anyo: 2022, marca: "Ford", modelo: "Fiesta", tipo_combustible: "Gasolina sin plomo", consumo_medio: 13.0},
 ]
--- src/app/acceptance/user.spec.ts
+++ src/app/acceptance/user.spec.ts
@@ -1,139 +1,20 @@
 import {TestBed} from '@angular/core/testing'
-import {MOCK_USERS} from './test-data';
+import {USER_TEST_DATA} from './test-data';
+import {USER_REPOSITORY} from '../services/User/UserRepository';
 import {UserModel} from '../data/UserModel';
 import {UserService} from '../services/User/user.service';
+import {UserDB} from '../services/User/UserDB';
 import {UserNotFoundError} from '../errors/UserNotFoundError';
 import {WrongPasswordFormatError} from '../errors/WrongPasswordFormatError';
 import {SessionNotActiveError} from '../errors/DBAccessError';
-import {USER_REPOSITORY} from '../services/User/UserRepository';
-import {UserDB} from '../services/User/UserDB';
+import {AccountNotFoundError} from '../errors/AccountNotFoundError';
 
-let userService: UserService;
-let ramon: UserModel;
-let maria: UserModel;
-let usuarios: UserModel[];
 
-// it01: HU101, HU102, HU105, HU603
+// it01: HU101, HU102, HU105, HU106, HU603
+describe('Pruebas sobre usuarios', () => {
+    let userService: UserService;
+    let ramon;
 
-describe('HU101: Registrar Usuario', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [UserService,
-                {provide: USER_REPOSITORY, useClass: UserDB}]
-        }).compileComponents();
-
-        userService = TestBed.inject(UserService);
-
-        // usuario "ramon"
-        usuarios = [...MOCK_USERS]
-        ramon = usuarios[0];
-        // lista de usuarios que no contenga a ramón
-        usuarios.splice(0,1);
-    });
-
-    it('HU101-EV01: Registrar nuevo usuario válido', async() => {
-        // GIVEN
-        //  lista de usuarios registrados en la que el usuario "ramon" no existe (hecho en beforeEach)
-        expect(usuarios).not.toContain(ramon);
-        //  session es null
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
-        //  datos del usuario válidos (hecho en beforeEach)
-
-        // WHEN
-        //  el usuario intenta darse de alta
-        await userService.signUp(ramon);
-
-        // THEN
-        //  el usuario se registra correctamente
-        //  lista de usuarios registrados ahora incluye al usuario
-        const exists = await userService.userExists(ramon);
-        expect(exists).toBe(true);
-    });
-
-    it('HU101-EI01: Registrar nuevo usuario con contraseña inválida', async () => {
-        // GIVEN
-        //  lista de usuarios registrados no incluye a 'ramón' (hecho en beforeEach)
-        expect(usuarios).not.toContain(ramon);
-        //  sesión null
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
-        //  datos del usuario NO válidos
-        ramon.password = "password"; // no tiene mayúscula, ni número, ni símbolo especial
-        expect(usuarios).not.toContain(ramon); // comprobación extra después de editar el objeto ramon
-
-        // WHEN
-        //  ramon intenta darse de alta con una contraseña que no sigue la regla
-        const res = userService.signUp(ramon);
-
-        // THEN
-        //  el usuario no se registra y se lanza el error WrongPasswordFormatError
-        await expectAsync(res).toBeRejectedWith(WrongPasswordFormatError);
-
-        //  lista de usuarios registrados no incluye al usuario
-        const exists = await userService.userExists(ramon);
-        expect(exists).toBe(false);
-    });
-});
-
-describe('HU102: Iniciar sesión', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [UserService,
-                {provide: USER_REPOSITORY, useClass: UserDB}]
-        }).compileComponents();
-
-        userService = TestBed.inject(UserService);
-
-        usuarios = [...MOCK_USERS];
-        ramon = usuarios[0];
-        maria = usuarios[1];
-    });
-    it('HU102-EV01: Iniciar sesión con una cuenta registrada', async () => {
-        // GIVEN
-        //  lista de usuarios registrados incluye a maría y ramón (hecho en BeforeEach)
-        expect(usuarios).toContain(ramon);
-        expect(usuarios).toContain(maria);
-
-        //  un atributo session null
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
-
-        // WHEN
-        // ramon intenta iniciar sesión con su usuario/email y contraseña
-        await userService.login(ramon);
-
-        // THEN
-        //  el usuario existe en la lista de usuarios
-        const exists = await userService.userExists(ramon);
-        expect(exists).toBe(true);
-    });
-
-    it('HU102-EI01: Iniciar sesión con una cuenta que no existe', async () => {
-        // GIVEN
-        // session es null
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
-
-        //  lista de usuarios registrados no incluye a ramón
-        usuarios.splice(0,1);
-        expect(usuarios).not.toContain(ramon);
-        let priorState = [...usuarios];
-
-        // WHEN
-        //  ramón intenta iniciar sesión sin figurar en la lista
-        const login = userService.login(ramon);
-
-        // THEN
-        //  se lanza el error UserNotFoundError
-        await expectAsync(login).toBeRejectedWith(UserNotFoundError);
-
-        //  no se modifica el estado
-        expect(priorState).toEqual(usuarios);
-    });
-})
-
-describe('HU105: Cerrar sesión', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [UserService,
@@ -143,81 +24,189 @@ describe('HU105: Cerrar sesión', () => {
         userService = TestBed.inject(UserService);
     });
 
-    it('HU105-EV01: Cerrar una sesión activa', async () => {
-        // GIVEN
-        //  sesión activa con el usuario "ramon" (hecho BeforeEach)
-        await expectAsync(userService.currentUser()).toBeResolvedTo(ramon);
+    describe('HU101: Registrar Usuario', () => {
 
-        // WHEN
-        //  el usuario “ramon” intenta cerrar sesión
-        const login = userService.logout();
+        it('HU101-EV01: Registrar nuevo usuario válido', async() => {
+            // GIVEN
+            //  lista de usuarios registrados vacía que no incluye a "ramon"
+            //  no se ha iniciado sesión
 
-        // THEN
-        //  no se lanza ningún error
-        await expectAsync(login).toBeResolved();
+            // WHEN
+            //  el usuario intenta darse de alta
+            ramon = USER_TEST_DATA[0];
+            const usuarioCreado : UserModel = await userService.signUp(ramon.email, ramon.pwd, ramon.nombre, ramon.apellidos);
 
-        //  se invalida la sesión
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
+            // THEN
+            //  el usuario se registra correctamente
+            expect(usuarioCreado).toEqual(jasmine.objectContaining({
+                uid: "???",
+                email: "ramonejemplo@gmail.com",
+                nombre: "Ramón",
+                apellidos: "García García"
+            }));
+
+            // la base de datos vuelve al estado inicial
+            await userService.deleteUser(usuarioCreado);
+        });
+
+        it('HU101-EI01: Registrar nuevo usuario con contraseña inválida', async () => {
+            // GIVEN
+            //  lista de usuarios registrados vacía que no incluye a "ramon"
+            //  no se ha iniciado sesión
+
+            // WHEN
+            //  el usuario intenta darse de alta con contraseña "password" (no sigue el formato correcto)
+            ramon = USER_TEST_DATA[0];
+            await expectAsync(userService.signUp(ramon.email, "password", ramon.nombre, ramon.apellidos)).toBeRejectedWith(new WrongPasswordFormatError());
+            // THEN
+            //  el usuario no se registra y se lanza el error WrongPasswordFormatError
+        });
     });
 
-    it('HU105-EI01: Cerrar una sesión cuando la sesión ya está cerrada', async () => {
-        // GIVEN
-        /*  el usuario “ramon” tiene una segunda ventana de cuando tenía sesión válida
-            (asumimos que el test se llama desde esta), pero el atributo session ya es null */
-        const nullCurrentUserPromise = await userService.currentUser();
-        expect(nullCurrentUserPromise).toBeNull();
 
-        // WHEN
-        //  el usuario “ramon” intenta cerrar sesión
-        const login = userService.logout();
+    describe('HU102: Iniciar sesión', () => {
 
-        // THEN
-        //  se lanza el error SessionNotActiveError
-        await expectAsync(login).toBeRejectedWith(SessionNotActiveError);
+        it('HU102-EV01: Iniciar sesión con una cuenta registrada', async () => {
+            // GIVEN
+            //  lista de usuarios registrados que incluye a "ramon"
+            //  no se ha iniciado sesión
+            ramon = USER_TEST_DATA[0];
+            const usuarioCreado = await userService.signUp(ramon.email, ramon.pwd, ramon.nombre, ramon.apellidos);
 
-        //  no se modifica el estado
-        const nullSecondCurrentUserPromise = await userService.currentUser();
-        expect(nullSecondCurrentUserPromise).toBeNull();
-    });
-});
+            // WHEN
+            // ramon intenta iniciar sesión con su email y contraseña
+            const respuesta = await userService.login(ramon.email, ramon.pwd);
 
+            // THEN
+            //  se inicia sesión correctamente
+            expect(respuesta).toBeTrue();
 
-describe('HU603: Guardar datos de usuarios', async () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [UserService,
-                {provide: USER_REPOSITORY, useClass: UserDB}]
-        }).compileComponents();
+            // la base de datos vuelve al estado inicial
+            await userService.deleteUser(usuarioCreado);
+        });
 
-        userService = TestBed.inject(UserService);
+        it('HU102-EI01: Iniciar sesión con una cuenta que no existe', async () => {
+            // GIVEN
+            //  lista de usuarios registrados vacía que no incluye a "ramon"
+            //  no se ha iniciado sesión
 
-        usuarios = [...MOCK_USERS];
-        ramon = usuarios[0];
+            // WHEN
+            // ramon intenta iniciar sesión con su email y contraseña
+            ramon = USER_TEST_DATA[0];
+            await expectAsync(userService.login(ramon.email, ramon.pwd)).toBeRejectedWith(UserNotFoundError);
+            // THEN
+            //  el usuario no se registra y se lanza el error UserNotFoundError
+        });
     })
 
-    it('HU603-EV01: Protección ante cierre involuntario cuando se pueden volcar los datos', async () => {
-        // GIVEN
-        //  conexión con la BD estable
-        //  el atributo session pasa a ser null habiendo iniciado sesión
-        await userService.login(ramon);
-        await expectAsync(userService.currentUser()).toBeResolvedTo(ramon);
 
-        const currentRamon = await userService.currentUser();
+    describe('HU105: Cerrar sesión', () => {
 
-        await userService.logout();
+        it('HU105-EV01: Cerrar una sesión activa', async () => {
+            // GIVEN
+            //  lista de usuarios registrados que incluye a "ramon"
+            //  sesión activa con el usuario "ramon"
+            ramon = USER_TEST_DATA[0];
+            const usuarioCreado = await userService.signUp(ramon.email, ramon.pwd, ramon.nombre, ramon.apellidos);
+            // todo: preguntar si deberíamos comprobar que se haya creado el usuario y se haya iniciado sesión correctamente
+            const sesionIniciada = await userService.login(ramon.email, ramon.pwd);
 
-        const nullUserPromise = await userService.currentUser();
-        expect(nullUserPromise).toBeNull();
+            // WHEN
+            //  se intenta cerrar sesión
+            const respuesta = userService.logout();
 
-        // WHEN
-        //  ramón vuelve a iniciar sesión
-        const logInPromise = userService.login(ramon);
-        await expectAsync(logInPromise).toBeResolved();
+            // THEN
+            //  no se lanza ningún error y se invalida la sesión
+            expect(respuesta).toBeTrue();
 
-        // THEN
-        //  los datos de usuario de la BD son los mismos que los introducidos previamente
-        const currentUserPromise = userService.currentUser();
-        await expectAsync(currentUserPromise).toBeResolvedTo(currentRamon);
+            // la base de datos vuelve al estado inicial
+            await userService.deleteUser(usuarioCreado);
+        });
+
+        it('HU105-EI01: Cerrar una sesión cuando no hay sesión activa', async () => {
+            // GIVEN
+            //  no hay ninguna sesión activa
+
+            // WHEN
+            //  se intenta cerrar sesión
+            await expectAsync(userService.logout()).toBeRejectedWith(SessionNotActiveError);
+            // THEN
+            //  se lanza el error SessionNotActiveError y no se cierra la sesión
+        });
     });
+
+
+    describe('HU106: Eliminar cuenta', async () => {
+
+        it('HU106-EV01: Eliminar una cuenta existente', async () => {
+            // GIVEN
+            //  el usuario "ramon" está registrado y ha iniciado sesión
+            ramon = USER_TEST_DATA[0];
+            const usuarioCreado = await userService.signUp(ramon.email, ramon.pwd, ramon.nombre, ramon.apellidos);
+            const sesionIniciada = await userService.login(ramon.email, ramon.pwd);
+
+            // WHEN
+            //  se intenta eliminar la cuenta
+            const usuarioBorrado = userService.deleteUser(usuarioCreado);
+
+            // THEN
+            //  no se lanza ningún error, se cierra sesión y se elimina la cuenta
+            expect(usuarioBorrado).toEqual(jasmine.objectContaining({
+                uid: "???",
+                email: "ramonejemplo@gmail.com",
+                nombre: "Ramón",
+                apellidos: "García García"
+            }));
+        });
+
+        it('HU106-EI01: Eliminar una cuenta que no existe', async () => {
+            // GIVEN
+            //  lista de usuarios registrados vacía que no incluye a "ramon"
+            //  no se ha iniciado sesión
+
+            // WHEN
+            //  se intenta eliminar la cuenta
+            const ramon = {
+                uid: "???",
+                email: "ramonejemplo@gmail.com",
+                nombre: "Ramón",
+                apellidos: "García García"
+            }
+            await expectAsync(userService.deleteUser(ramon)).toBeRejectedWith(new AccountNotFoundError());
+            // THEN
+            //  se lanza el error AccountNotFoundError y no se elimina ninguna cuenta
+        });
+    });
+
+/*
+    describe('HU603: Guardar datos de usuarios', async () => {
+
+        it('HU603-EV01: Protección ante cierre involuntario cuando se pueden volcar los datos', async () => {
+            // GIVEN
+            //  conexión con la BD estable
+            //  el atributo session pasa a ser null habiendo iniciado sesión
+            ramon = USER_TEST_DATA[0];
+            await userService.login(ramon.email, ramon.pwd);
+            await expectAsync(userService.getCurrentUser()).toBeResolvedTo(ramon);
+
+            const currentRamon = await userService.getCurrentUser();
+
+            await userService.logout();
+
+            const nullUserPromise = await userService.getCurrentUser();
+            expect(nullUserPromise).toBeNull();
+
+            // WHEN
+            //  ramón vuelve a iniciar sesión
+            const logInPromise = userService.login(ramon);
+            await expectAsync(logInPromise).toBeResolved();
+
+            // THEN
+            //  los datos de usuario de la BD son los mismos que los introducidos previamente
+            const currentUserPromise = userService.getCurrentUser();
+            await expectAsync(currentUserPromise).toBeResolvedTo(currentRamon);
+        });
+    })
+
+ */
 })

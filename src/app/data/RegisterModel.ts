80f2f6be282a82faedebbaae3d6d138050fc927d
Manuel <al426179@uji.es>
2025-11-18 13:46:43 +0100

    feat(it01hu101): v1 of interface. missing firebase functionalities

---
+++ src/app/data/RegisterModel.ts
@@ -0,0 +1,13 @@
+export class RegisterModel {
+    email: string;
+    nombre: string;
+    apellidos: string;
+    pwd: string;
+
+    constructor(email: string, nombre: string, apellidos: string, password: string) {
+        this.email = email;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.pwd = password;
+    }
+}

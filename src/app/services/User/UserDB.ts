62db787bb69c4b4459eaf5678a89474d52a8b4f5
Daniel Ortiz <al426198@uji.es>
2025-11-20 10:41:21 +0100

    feaat(it01hu105): model done, missing UI

--- src/app/services/User/UserDB.ts
+++ src/app/services/User/UserDB.ts
@@ -1,8 +1,9 @@
 import {UserModel} from '../../data/UserModel';
 import {UserRepository} from './UserRepository';
-import {deleteUser, Auth} from "@angular/fire/auth";
-import {SessionNotActiveError} from '../../errors/SessionNotActiveError';
+import {Auth} from "@angular/fire/auth";
 import {inject} from '@angular/core';
+import {SessionNotActiveError} from '../../errors/SessionNotActiveError';
+import {UserNotFoundError} from '../../errors/UserNotFoundError';
 
 export class UserDB implements UserRepository {
     private auth: Auth = inject(Auth)
@@ -10,19 +11,30 @@ export class UserDB implements UserRepository {
     async createUser(email: string, pwd: string, nombre: string, apellidos: string) : Promise<UserModel> {
         return {uid:"", email: "", nombre:"", apellidos:""};
     }
-    async deleteUser() : Promise<boolean> {
-        // Obtiene el usuario de la sesión; si no hay, no se puede borrar
+
+    async validateCredentials(email: string, password: string): Promise<boolean> {return false;}
+
+    /**
+     * Intenta cerrar sesión en Firebase.
+     * @returns Promise con true si se ha podido cerrar sesión; false en caso de excepción.
+     */
+    async logoutUser(): Promise<boolean> {
+        // Obtiene el usuario de la sesión; si no hay, ya se ha cerrado la sesión
         const user = this.auth.currentUser;
         if (!user) throw new SessionNotActiveError();
 
-        // Borra al usuario de la BD, y cierra la sesión
-        // Si falla, se atrapa la excepción y no se borra el usuario
-        deleteUser(user).catch((error) => {
+        // Cierra la sesión del usuario
+        this.auth.signOut().catch((error) => {
+
+            // El usuario ya no existe (eliminar y cerrar sesión en pestañas distintas)
+            if (error.code == 'auth/invalid-credential') {
+                throw new UserNotFoundError();
+            }
+
+            // Error cualquiera
             console.error('ERROR de Firebase al borrar usuario: ' + error);
             return false;
         });
         return true;
     }
-
-    async validateCredentials(email: string, password: string): Promise<boolean> {return false;}
 }

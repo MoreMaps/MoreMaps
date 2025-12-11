321739916626de1ece962dff38d651a235be5578
Manuel <al426179@uji.es>
2025-12-11 19:37:34 +0100

    feat(it04hu401): changed add-button in  navbar to open a menu instead

--- src/app/view/navbar/navbar.component.ts
+++ src/app/view/navbar/navbar.component.ts
@@ -1,17 +1,27 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router, RouterModule} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {AddPoiDialogComponent, AddPoiMethod} from './add-poi-dialog/add-poi-dialog';
 import {CoordsSearchDialogComponent} from './coords-search-dialog/coords-search-dialog';
-import { MapUpdateService } from '../../services/map-update-service/map-updater';
-import {PlaceNameSearchDialogComponent} from './placename-search-dialog/placename-search-dialog'; // Asegura la ruta correcta
+import {MapUpdateService} from '../../services/map-update-service/map-updater';
+import {PlaceNameSearchDialogComponent} from './placename-search-dialog/placename-search-dialog';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar'; // Asegura la ruta correcta
 
 @Component({
     selector: 'app-navbar',
-    imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule, MatDialogModule],
+    imports: [
+        CommonModule,
+        RouterModule,
+        MatIconModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatMenuModule,
+        MatSnackBarModule,
+    ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css'
 })
@@ -19,6 +29,7 @@ export class NavbarComponent {
     private router = inject(Router);
     private dialog = inject(MatDialog);
     private mapUpdateService = inject(MapUpdateService);
+    private snackBar = inject(MatSnackBar);
 
     isActive(route: string): boolean {
         return this.router.url.split(/[?#]/)[0] === route;
@@ -28,6 +39,9 @@ export class NavbarComponent {
         this.router.navigate([route]);
     }
 
+    // --- Funciones del menú ---
+
+    // 1. Búsqueda de POI
     openAddDialog(): void {
         // Abrir el diálogo
         const dialogRef = this.dialog.open(AddPoiDialogComponent, {
@@ -50,6 +64,18 @@ export class NavbarComponent {
         });
     }
 
+    // 2. Búsqueda de rutas
+    openRouteSearch(): void {
+        this.snackBar.open('Función no implementada', 'OK', {
+            duration: 3000,
+            horizontalPosition: 'left',
+            verticalPosition: 'bottom'
+        });
+    }
+
+    // --- Funciones de búsqueda de POI ---
+
+    // 1a. Búsqueda por coordenadas
     openCoordsDialog(): void {
         const coordsDialogRef = this.dialog.open(CoordsSearchDialogComponent, {
             width: '90%',
@@ -68,6 +94,7 @@ export class NavbarComponent {
         });
     }
 
+    // 1b. Búsqueda por topónimo
     openPlaceNameDialog(): void {
         const placeNameDialogRef = this.dialog.open(PlaceNameSearchDialogComponent, {
             width: '90%',

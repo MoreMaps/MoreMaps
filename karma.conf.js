5d47c49c3e529c72cc37614019c742b7cf1d4bb7
Manuel <al426179@uji.es>
2026-01-02 20:00:26 +0100

    tests(it05): increased default nonCI timeout for good measure

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
         // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
-          timeout: isCI ? 20000 : 5000
+          timeout: isCI ? 20000 : 10000
       },
     },
 
